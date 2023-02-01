3c25e0360dffbaadddc44c3beb733fe5d959a46c
garciavelezjessica <garciavelezjessica@gmail.com>
2023-02-01 12:28:35 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ex3
+
+Favorite quotation:
+
+"If you can dream it, you can do it."
---
+++ index.js
@@ -0,0 +1,10 @@
+let quotes = 
+[
+    "Keep smiling", 
+    "Life is either a daring lesson or nothing at all", 
+    "It is during our darkest moment that we must focus to see the light."
+];
+
+let quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+console.log(quote);
